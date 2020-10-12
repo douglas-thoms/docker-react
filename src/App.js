30d93a4b0dc575930d4da12a5b67a34c8c58b00a
Douglas Thoms <douglasthoms@rodanenergy.com>
2020-10-12 19:04:15 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          No, YOUR mama
+          No, YOUR papa
         </p>
         <a
           className="App-link"
